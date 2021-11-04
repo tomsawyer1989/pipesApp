7291b36b6d0ee78740b8eeaa2fde0cf9fe481184
Jorge Leonardo Herrera Ortiz <jorge_skidrow@hotmail.com>
2021-11-03 23:05:51 -0500

    feat(PrimeNg - Menubar)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
-import { PrimeNgModule } from './prime-ng/prime-ng.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -11,7 +11,7 @@ import { PrimeNgModule } from './prime-ng/prime-ng.module';
   ],
   imports: [
     BrowserModule,
-    PrimeNgModule
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from "@angular/common";
 
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+import { MenubarModule } from 'primeng/menubar';
 
 @NgModule({
     exports: [
         CommonModule,
         ButtonModule,
-        CardModule
+        CardModule,
+        MenubarModule
     ]
 })
 export class PrimeNgModule {}
