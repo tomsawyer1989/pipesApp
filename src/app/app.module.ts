75da9346aceca99d2b020e6f2cb6790e50cb6df8
Jorge Leonardo Herrera Ortiz <jorge_skidrow@hotmail.com>
2022-01-16 22:29:01 -0500

    feat(PrimeFlex)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 
 import { AppRouterModule } from './app-router.module';
 import { SharedModule } from './shared/shared.module';
+import { VentasModule } from './ventas/ventas.module';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     AppRouterModule,
-    SharedModule
+    SharedModule,
+    VentasModule
   ],
   providers: [],
   bootstrap: [AppComponent]
