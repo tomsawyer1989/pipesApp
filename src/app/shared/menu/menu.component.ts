8f6ebedc94a9f91eb3b60de7c7e8141ff092d9f2
Jorge Leonardo Herrera Ortiz <jorge_skidrow@hotmail.com>
2022-01-16 21:46:04 -0500

    feat(Cambiar las rutas utilizando el MenuBar)

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -11,26 +11,29 @@ export class MenuComponent implements OnInit {
     ngOnInit() {
         this.items = [
             {
-                label: 'File',
-                items: [{
-                        label: 'New', 
-                        icon: 'pi pi-fw pi-plus',
-                        items: [
-                            {label: 'Project'},
-                            {label: 'Other'},
-                        ]
+                label: 'Pipes de Angular',
+                icon: 'pi pi-desktop',
+                items: [
+                    {
+                        label: 'Textos y Fechas',
+                        icon: 'pi pi-align-left',
+                        routerLink: '/'
                     },
-                    {label: 'Open'},
-                    {label: 'Quit'}
+                    {
+                        label: 'Números',
+                        icon: 'pi pi-dollar',
+                        routerLink: 'numeros'
+                    },
+                    {
+                        label: 'No comunes',
+                        icon: 'pi pi-globe',
+                        routerLink: 'no-comunes'
+                    },
                 ]
             },
             {
-                label: 'Edit',
-                icon: 'pi pi-fw pi-pencil',
-                items: [
-                    {label: 'Delete', icon: 'pi pi-fw pi-trash'},
-                    {label: 'Refresh', icon: 'pi pi-fw pi-refresh'}
-                ]
+                label: 'Pipes personalizados',
+                icon: 'pi pi-cog',
             }
         ];
     }
