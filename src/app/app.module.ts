bea0e82310ab3f7ea752cdb5a062af4900718df4
Jorge Leonardo Herrera Ortiz <jorge_skidrow@hotmail.com>
2022-01-17 00:06:53 -0500

    feat(Timezone y otros idiomas)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,10 @@ import { VentasModule } from './ventas/ventas.module';
 
 // Cambiar el locale de la app
 import localeEs from '@angular/common/locales/es-HN';
+import localeFr from '@angular/common/locales/fr';
 import { registerLocaleData } from '@angular/common';
 registerLocaleData(localeEs);
+registerLocaleData(localeFr);
 
 @NgModule({
   declarations: [
