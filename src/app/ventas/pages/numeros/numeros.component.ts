880a8aea88f50aab907692d90a3e5ff28809e6cb
Jorge Leonardo Herrera Ortiz <jorge_skidrow@hotmail.com>
2022-01-17 00:42:35 -0500

    feat(DecimalPipe)

--- src/app/ventas/pages/numeros/numeros.component.ts
+++ src/app/ventas/pages/numeros/numeros.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-numeros',
@@ -6,11 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class NumerosComponent implements OnInit {
+export class NumerosComponent {
+  ventasNetas: number = 2567789.556759;
+  porcentaje: number = 0.48;
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
